5d6ef4f39afd93952a4ff3c36f3a0fdce933bdf4
unknown <osama_magdy0@yahoo.com>
2022-12-12 06:39:09 +0200

    i will at my office

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let weather = {
-  apiKey: "1e32f989fd14e648f943adf84688d633",
-};
+// let weather = {
+//   apiKey: "1e32f989fd14e648f943adf84688d633",
+//   fetchWeather: function (city) {
+//     fetch(
+//       "https://api.openweathermap.org/data/2.5/weather?q=" +
+//         city +
+//         "&units=metric=" +
+//         this.apiKey
+//     )
+//       .then((response) => {
+//         response.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//       });
+//   },
+// };
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=London&appid=1e32f989fd14e648f943adf84688d633`
+).then((res) => {
+  console.log(res);
+});
